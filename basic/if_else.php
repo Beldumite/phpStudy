80cf32587ae6eb0abae318abe5290037161e0734
Kutuputih <azkaarrofiq12@gmail.com>
2025-05-01 17:22:08 +0700

    update

---
+++ basic/if_else.php
@@ -0,0 +1,27 @@
+<?php 
+
+$x = 5;
+$y = 4;
+
+if ($x > $y) {
+    echo "$x is bigger than $y";
+} else {
+    echo "$x is smaller than $y";
+}
+// yeah its the same thing as other if statement
+
+//if oneliner
+
+$a = 5;
+
+if ($a < 10) $b = "Hello";
+
+echo $b;
+    
+// if-else oneliner
+
+$c = 2;
+
+$d = $c > 10 ? "hello" : "hi";
+
+echo $d;
